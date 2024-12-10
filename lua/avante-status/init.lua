9738b01b510b9323136a6d0577c4caeb7e3efa84
takeshid <goriponikeike55@gmail.com>
2024-12-10 14:39:17 +0900

    modified providers

--- lua/avante-status/init.lua
+++ lua/avante-status/init.lua
@@ -1,29 +1,43 @@
 -- ########### Require Library #############
-local Path = require("plenary.path")
-
-local M = {}
-M.current_chat_provdier = nil
-M.current_suggestions_provdier = nil
+local M = {
+    chat_provider = {
+        type = "none",
+        value = "none",
+        icon = "",
+        highlight = "",
+        fg = "#ffffff",
+        name = "None",
+    },
+    suggestions_provider = {
+        type = "none",
+        value = "none",
+        icon = "",
+        highlight = "",
+        fg = "#ffffff",
+        name = "None",
+    }
+}
 
 ---Returns true if the environment variable envname exists, false if it does not exist.
 ---@param envname string
 ---@return boolean
 local exist_envname = function(envname)
-    return vim.fn.getenv(envname) ~= vim.NIL
+    return vim.fn.getenv(envname) ~= vim.v.null
 end
 
 ---Returns true if the path exists, false if it does not exist
 ---@param path string
 ---@return boolean
 local exist_path = function(path)
-    return Path:new(vim.fn.expand(path)):exists()
+    -- return Path:new(vim.fn.expand(path)):exists()
+    return vim.uv.fs_stat(path) ~= nil
 end
 
 ---Returns T if cond is true, returns F if cond is false
 ---@param cond boolean
 ---@param T any
 ---@param F any
----@return any
+---@return any;;
 local ternary = function(cond, T, F)
     if cond then return T else return F end
 end
@@ -38,40 +52,13 @@ M.getenv_if = function(envname, F)
     return ternary(exist_envname(envname), vim.fn.getenv(envname), F)
 end
 
-local bg_statusline = vim.api.nvim_get_hl(0, { name = "StatusLine"}).bg
-local Highlights = {
-    AZURE   = { name = "AvanteIconAzure",   fg = "#008ad7"},
-    CLAUDE  = { name = "AvanteIconClaude",  fg = "#d97757"},
-    OPENAI  = { name = "AvanteIconOpenAI",  fg = "#76a89c"},
-    COPILOT = { name = "AvanteIconCopilot", fg = "#cccccc"},
-    GEMINI  = { name = "AvanteIconGemini",  fg = "#3a92db"},
-    COHERE  = { name = "AvanteIconCohere",  fg = "#d2a1de"},
-}
-
-local function has_set_colors(hl_group)
-  local hl = vim.api.nvim_get_hl(0, { name = hl_group })
-  return next(hl) ~= nil
-end
-
-M.setup_highlight = function()
-    vim
-      .iter(Highlights)
-      :filter(function(k, _)
-        return k:match("^%u+_") or k:match("^%u+$")
-      end)
-      :each(function(_, hl)
-        if not has_set_colors(hl.name) then
-          vim.api.nvim_set_hl(0, hl.name, { fg = hl.fg or nil, bg = hl.bg or nil, link = hl.link or nil })
-        end
-      end)
-end
-
 local provider_value_map = {
     azure = {
         type = "envvar",
         value = "AZURE_OPENAI_API_KEY",
         icon = "",
         highlight = "AvanteIconAzure",
+        fg = "#008ad7",
         name = "Azure",
     },
     claude = {
@@ -79,6 +66,7 @@ local provider_value_map = {
         value = "ANTHROPIC_API_KEY",
         icon = "󰛄",
         highlight = "AvanteIconClaude",
+        fg = "#d97757",
         name = "Claude",
     },
     openai = {
@@ -86,6 +74,7 @@ local provider_value_map = {
         value = "OPENAI_API_KEY",
         icon = "",
         highlight = "AvanteIconOpenAI",
+        fg = "#76a89c",
         name = "OpenAI",
     },
     copilot = {
@@ -93,6 +82,7 @@ local provider_value_map = {
         value = vim.fn.stdpath("data") .. "/avante/github-copilot.json",
         icon = "",
         highlight = "AvanteIconCopilot",
+        fg = "#cccccc",
         name = "Copilot",
     },
     gemini = {
@@ -100,6 +90,7 @@ local provider_value_map = {
         value = "GEMINI_API_KEY",
         icon = "󰫢",
         highlight = "AvanteIconGemini",
+        fg = "#3a92db",
         name = "Gemini",
     },
     cohere = {
@@ -107,7 +98,8 @@ local provider_value_map = {
         value = "CO_API_KEY",
         icon = "󰺠",
         highlight = "AvanteIconCohere",
-        name = "Cohere",
+        fg = "#008ad7",
+        name = "#d2a1de",
     }
 }
 
@@ -128,45 +120,29 @@ local get_provider = function(providers, provider_type)
             return tostring(provider)
         end
     end
-    error("The provider for which the api-key is set cannot be obtained.")
+    local unavailable_providers = vim.iter(providers):fold("", function(p1, p2) return p1 .. ", " .. p2 end)
+    error("'" .. unavailable_providers .. "' for which the api-key is set cannot be obtained.")
     return ""
 end
 
----Returns the provider that has the first environment variable set among the providers. 
+---Returns the provider that has the first environment variable set among the providers.
 ---The provider that is set at the top has priority, so the list is sorted to set the priority.
 ---@param providers string[]
 ---@return string
-M.get_chat_provider = function(providers)
+function M.get_chat_provider(providers)
     local provider = get_provider(providers, "chat")
-    M.current_chat_provider = provider_value_map[provider]
-    M.setup_highlight()
+    M.chat_provider = provider_value_map[provider]
     return provider
 end
 
----Returns the provider that has the first environment variable set among the providers. 
+---Returns the provider that has the first environment variable set among the providers.
 ---The provider that is set at the top has priority, so the list is sorted to set the priority.
 ---@param providers string[]
 ---@return string
-M.get_suggestions_provider = function(providers)
+function M.get_suggestions_provider(providers)
     local provider = get_provider(providers, "suggestions")
-    M.current_suggestions_provider = provider_value_map[provider]
-    M.setup_highlight()
+    M.suggestions_provider = provider_value_map[provider]
     return provider
 end
-
-M.setup = function(opts)
-    M.setup_highlight()
-end
-
-setmetatable(M, {
-  __index = function(t, k)
-    if Highlights[k] ~= nil then
-      return Highlights[k].name
-    elseif Highlights.conflict[k] ~= nil then
-      return Highlights.conflict[k].name
-    end
-    return t[k]
-  end,
-})
 
 return M
---
+++ plugin/avante-status.vim
@@ -0,0 +1,4 @@
+if exists('g:loaded_avantestatus')
+    finish
+endif
+let g:loaded_avantestatus = 1
